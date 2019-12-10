d13bd43b245b0ff0ed132d1e240f474b06fb07b0
KrishSrivatsav <57484094+KrishSrivatsav@users.noreply.github.com>
2019-12-10 21:26:09 +0530

    Add files via upload

---
+++ Circle/circle.js
@@ -0,0 +1,22 @@
+u=20;
+function upbutt(){
+    document.getElementById("control").style.marginTop="-"+u+"px";
+    console.log("upbutt");
+    U++
+}
+//  upbutt();
+// d=50;
+ function downbutt(){
+    document.getElementById("control").style.marginTop=u+"px";
+    console.log("downbutt");
+}
+l=20;
+function leftbutt(){
+    document.getElementById("control").style.marginLeft="-"+l+"px";
+    console.log("leftbutt");
+}
+r=50;
+function rightbutt(){
+    document.getElementById("control").style.marginRight="-"+r+"px";
+    console.log("rightbutt");
+}
